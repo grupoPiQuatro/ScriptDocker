843d4d9e0c22233c97a11493eb86d1a945ed9b14
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2023-05-09 20:48:41 -0300

    script editado

--- run.sh
+++ run.sh
@@ -2,19 +2,40 @@
 
 echo verificando se o docker está instalado...
 docker --version
-sleep 2
 if  [ $? -eq 0 ]
 	then
-		echo iniciando banco de dados...
-		sleep 2
-		docker compose up -d
-		echo container do banco iniciado!!
-		sleep 2
-		echo tabelas criadas
+        docker compose version
+        if [ $? -eq 0 ]
+            echo iniciando banco de dados...
+            sleep 2
+            docker compose up -d
+            echo container do banco iniciado!!
+            sleep 2
+            echo tabelas criadas
+        else
+            echo instalando docker compose...
+            sleep 2
+            echo criando repositorio local docker...
+            sleep 2
+            sudo apt install ca-certificates curl gnupg lsb-release
+            mkdir /etc/apt/demokeyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/demokeyrings/demodocker.gpg
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/demokeyrings/demodocker.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+            sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+            sleep 2
+
+            echo iniciando banco de dados...
+            sleep 2
+            docker compose up -d
+            echo container do banco iniciado!!
+            sleep 2
+            echo tabelas criadas
 	else
-	        echo instalando o docker...
+        echo instalando o docker...
 		sleep 2
-                sudo apt install docker.io
+        sudo apt install docker.io
 		sleep 2
 		echo docker instalado consucesso!!
 		sudo gpasswd -a $USER docker
@@ -33,10 +54,10 @@ if  [ $? -eq 0 ]
 		sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 		sleep 2
 
-                echo iniciando banco de dados...
-                sleep 2
-                docker compose up -d
-                echo container do banco iniciado!!
-                sleep 2
-                echo tabelas criadas
+        echo iniciando banco de dados...
+        sleep 2
+        docker compose up -d
+        echo container do banco iniciado!!
+        sleep 2
+        echo tabelas criadas
 fi
