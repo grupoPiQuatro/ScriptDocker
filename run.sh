8c2979c2cc3445f3698f9f7c99c13c1aa12cdd12
Ubuntu <ubuntu@ip-172-31-31-118.ec2.internal>
2023-05-10 03:17:22 +0000

    Script de automação testado: instalando docker, instalando docker compose (para iniciar o container) e container banco de dados

--- run.sh
+++ run.sh
@@ -14,42 +14,46 @@ then
             sleep 2
             echo tabelas criadas
         else
-            echo instalando docker compose...
-            sleep 2
-            sudo apt-get update
-	    sudo apt-get install docker-compose-plugin
-	    sudo yum update
-	    sudo yum install docker-compose-plugin
-	    echo docker compose instalado
-	    docker compose version
-            sleep 2
-            echo iniciando banco de dados...
-            sleep 2
-            docker compose up -d
-            echo container do banco iniciado!!
-            sleep 2
-            echo tabelas criadas
+    		echo instalando docker compose...
+    		sleep 2
+    		sudo apt-get update
+		sudo apt-get install ca-certificates curl gnupg
+		sudo apt-get install docker-compose-plugin
+	    	DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+		sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+		sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+		sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+		sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+		echo docker compose instalado
+	    	docker compose version
+            	sleep 2
+            	echo iniciando banco de dados...
+            	sleep 2
+            	docker compose up -d
+            	echo container do banco iniciado!!
+            	sleep 2
+            	echo tabelas criadas
 	fi
 else
         echo instalando o docker...
 	sleep 2
-        sudo apt install docker.io
+        sudo apt install docker.io -y
 	sleep 2
 	echo docker instalado consucesso!!
 	sudo gpasswd -a $USER docker
 	newgrp docker
-
-	echo instalando docker compose...
-	sleep 2
     	echo instalando docker compose...
     	sleep 2
-    	sudo apt-get update
-    	sudo apt-get install docker-compose-plugin
-    	sudo yum update
-    	sudo yum install docker-compose-plugin
+        sudo apt-get update
+        sudo apt-get install ca-certificates curl gnupg
+        sudo apt-get install docker-compose-plugin
+        DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+        sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+        sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+        sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+        sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
     	echo docker compose instalado
     	docker compose version
-
         echo iniciando banco de dados...
         sleep 2
         docker compose up -d
