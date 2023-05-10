ae84d7a558dab5cc847ea4ca2e32f644ef7ced9a
Ubuntu <ubuntu@ip-172-31-31-118.ec2.internal>
2023-05-10 03:39:00 +0000

    corrigindo erros de identação

--- run.sh
+++ run.sh
@@ -14,25 +14,28 @@ then
             sleep 2
             echo tabelas criadas
         else
-    		echo instalando docker compose...
-    		sleep 2
-    		sudo apt-get update
-		sudo apt-get install ca-certificates curl gnupg
-		sudo apt-get install docker-compose-plugin
-	    	DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-		sudo mkdir -p $DOCKER_CONFIG/cli-plugins
-		sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-		sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-		sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-		echo docker compose instalado
-	    	docker compose version
-            	sleep 2
-            	echo iniciando banco de dados...
-            	sleep 2
-            	docker compose up -d
-            	echo container do banco iniciado!!
-            	sleep 2
-            	echo tabelas criadas
+	    sudo gpasswd -a $USER docker
+            newgrp docker
+	    echo instalando docker compose...
+    	    sleep 2
+    	    sudo apt-get update
+	    sudo apt-get install ca-certificates curl gnupg
+	    sudo apt-get install docker-compose-plugin
+	    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+	    sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+	    sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+	    sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+	    sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+	    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	    echo docker compose instalado
+	    docker compose version
+            sleep 2
+            echo iniciando banco de dados...
+            sleep 2
+            docker compose up -d
+            echo container do banco iniciado!!
+            sleep 2
+            echo tabelas criadas
 	fi
 else
         echo instalando o docker...
@@ -52,6 +55,7 @@ else
         sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
         sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
         sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     	echo docker compose instalado
     	docker compose version
         echo iniciando banco de dados...
